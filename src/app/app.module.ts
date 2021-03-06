af4111744831a5ec22f81bf52ad14af8b32ce486
roman-azami <nascentsoul13@gmail.com>
2021-03-06 18:45:23 +0300

    Install Angular Material component library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ElementOfArrayComponent } from './components/element-of-array/element-of-array.component';
 import { ArrayToBeSortedComponent } from './components/array-to-be-sorted/array-to-be-sorted.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { ArrayToBeSortedComponent } from './components/array-to-be-sorted/array-
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
