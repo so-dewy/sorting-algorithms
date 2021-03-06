65e02c20e2fc9d4acd6818752226969eb189686f
roman-azami <nascentsoul13@gmail.com>
2021-03-06 18:26:53 +0300

    Update Angular from 10 to 11

--- src/app/components/array-to-be-sorted/array-to-be-sorted.component.spec.ts
+++ src/app/components/array-to-be-sorted/array-to-be-sorted.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ArrayToBeSortedComponent } from './array-to-be-sorted.component';
 
@@ -6,7 +6,7 @@ describe('ArrayToBeSortedComponent', () => {
   let component: ArrayToBeSortedComponent;
   let fixture: ComponentFixture<ArrayToBeSortedComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ArrayToBeSortedComponent ]
     })
